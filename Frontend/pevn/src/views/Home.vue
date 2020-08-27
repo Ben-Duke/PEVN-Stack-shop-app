1a4e58a4fa290609343293d4efa1380a746c08f6
Ben Duke <benduke.nz@gmail.com>
2020-08-27 20:25:50 +1200

    Added product cards to home page with add to basket button

--- Frontend/pevn/src/views/Home.vue
+++ Frontend/pevn/src/views/Home.vue
@@ -1,10 +1,56 @@
 <template>
   <div class="home">
-    <ul id="example-1">
-  <li v-for="product in products" :key = "product.product_id">
-    {{ product.product_name }}
+    <!-- <ul id="example-1">
+<li v-for="product in products" :key = "product.product_id">
+<v-card flat dark height="100%">
+    <v-container fluid>
+      <v-layout column>
+        <v-flex>{{product.product_name}}</v-flex>
+        <v-flex>
+          <img v-bind:src="product.url" alt="product picture" width="250" height="200">
+        </v-flex>
+        <v-flex xs2 class="caption">
+          <span>
+            {{ product.product_price }}
+          </span>
+        </v-flex>
+  
+      </v-layout>
+    </v-container>
+  </v-card>    
+    
   </li>
   </ul>
+  
+   -->
+  <v-container fluid grid-list-md>
+      
+    <v-layout row wrap>
+      <v-flex xs12 md4 v-for="product in products" :key = "product.product_id">
+        <v-card flat deep-purple height="100%">
+    <v-container fluid>
+      <v-layout column>
+        <v-flex>{{product.product_name}}</v-flex>
+        <v-flex>
+          <img v-bind:src="product.url" alt="product picture" width="250" height="200">
+        </v-flex>
+        <v-flex xs2 class="caption">
+          <span>
+            {{ product.product_price }} 
+            <span>
+            <v-btn small>Add to basket</v-btn>
+            </span>
+          </span>
+          
+        </v-flex>
+  
+      </v-layout>
+    </v-container>
+  </v-card> 
+          </v-flex>
+      </v-layout>
+  </v-container>
+
   </div>
 </template>
 
