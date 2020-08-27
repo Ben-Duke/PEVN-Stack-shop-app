cefb9f1fd0f7a34f50fe5b9aa87dfbc145a89e31
Ben Duke <benduke.nz@gmail.com>
2020-08-27 22:43:06 +1200

    Added buttons ready to add functionality

--- Frontend/pevn/src/plugins/vuetify.js
+++ Frontend/pevn/src/plugins/vuetify.js
@@ -1,7 +1,19 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
-export default new Vuetify({
-});
+const vuetify = new Vuetify({
+  theme: {
+      light:true,
+    themes: {
+      light: {
+        primary: '#3f51b5',
+        secondary: '#b0bec5',
+        anchor: '#8c9eff',
+      },
+    },
+  },
+})
+
+export default vuetify
--- Frontend/pevn/src/views/Home.vue
+++ Frontend/pevn/src/views/Home.vue
@@ -1,35 +1,16 @@
+
 <template>
-  <div class="home">
-    <!-- <ul id="example-1">
-<li v-for="product in products" :key = "product.product_id">
-<v-card flat dark height="100%">
-    <v-container fluid>
-      <v-layout column>
-        <v-flex>{{product.product_name}}</v-flex>
-        <v-flex>
-          <img v-bind:src="product.url" alt="product picture" width="250" height="200">
-        </v-flex>
-        <v-flex xs2 class="caption">
-          <span>
-            {{ product.product_price }}
-          </span>
-        </v-flex>
-  
-      </v-layout>
-    </v-container>
-  </v-card>    
-    
-  </li>
-  </ul>
-  
-   -->
+
+<div class="home">
+
+
   <v-container fluid grid-list-md>
       
     <v-layout row wrap>
       <v-flex xs12 md4 v-for="product in products" :key = "product.product_id">
-        <v-card flat deep-purple height="100%">
+        <v-card >
     <v-container fluid>
-      <v-layout column>
+      <v-layout column >
         <v-flex>{{product.product_name}}</v-flex>
         <v-flex>
           <img v-bind:src="product.url" alt="product picture" width="250" height="200">
@@ -38,7 +19,7 @@
           <span>
             {{ product.product_price }} 
             <span>
-            <v-btn small>Add to basket</v-btn>
+            <v-btn small >Add to basket</v-btn>
             </span>
           </span>
           
@@ -47,14 +28,23 @@
       </v-layout>
     </v-container>
   </v-card> 
+  
           </v-flex>
+          
       </v-layout>
+      <v-btn small >previous 10 products</v-btn>
+      <v-btn small >next 10 products</v-btn>
   </v-container>
 
   </div>
 </template>
 
+
+
 <script>
+
+
+
 export default {  
   data() {
     return {
